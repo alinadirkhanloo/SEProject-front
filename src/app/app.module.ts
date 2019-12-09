10ea313c568f0b36a6b645507103ae1fe409c0e3
jolan <jolan657673@gmail.com>
2019-12-09 13:51:51 +0330

    create toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,38 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './pages/login/login.component';
+import { MatFormFieldModule,
+   MatInputModule,
+    MatCardModule,
+     MatButtonModule,
+      MatToolbarModule,
+       MatDialogModule,
+        MatTableModule,
+        MatMenuModule,
+         MatIconModule,
+          MatProgressSpinnerModule } from '@angular/material';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent,LoginComponent
+    AppComponent,LoginComponent, ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [LoginComponent],
