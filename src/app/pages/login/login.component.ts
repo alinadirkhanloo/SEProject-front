dd51415fcc77f91955f645c3f71a734ae2d3290d
jolan <jolan657673@gmail.com>
2019-12-23 17:40:35 +0330

    create blog page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  inputs:[`darkmode`]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sharedData:DataSharingService) { }
 
   ngOnInit() {
+    console.log(this.sharedData.darkmode)
   }
 
 }
